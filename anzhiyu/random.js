a59700cd297e9af8843a336274a3735a7e9b4589
xiaoerweb <xiaoerweb@gmail.com>
2025-02-09 01:24:47 +0800

    Site updated: 2025-02-09 01:24:46

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/02/09/这是一篇新的博文/","2025/02/09/hello-world/"];function toRandomPost(){
+var posts=["2025/02/09/hello-world/","2025/02/09/这是一篇新的博文/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
